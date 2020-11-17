ba37ac68531a725bc7f170bea1f25723a5663a91
RuslanZahriadskyi <67427154+RuslanZahriadskyi@users.noreply.github.com>
2020-11-17 17:36:47 +0100

    fix window listener

--- js/task.js
+++ js/task.js
@@ -21,11 +21,6 @@ const mainRefs = {
 wrapperRef.addEventListener('click', openModal);
 mainRefs.backDrop.addEventListener('click', onBackDropCloseModal);
 mainRefs.onCloseBtnRef.addEventListener('click', closeModal);
-window.addEventListener('keyup', closeModalByEsc);
-window.addEventListener('keyup', pressRightBtn);
-window.addEventListener('keyup', pressLeftBtn);
-mainRefs.btnRightArrowRef.addEventListener('click', onRightArrowClick);
-mainRefs.btnLeftArrowRef.addEventListener('click', onLeftArrowClick);
 
 function onCreateGallery(image, i) {
   const refs = {
@@ -67,12 +62,23 @@ function openModal(event) {
 
   mainRefs.backDropWrapper.classList.add('is-open');
   imgAttribute(getImg.dataset.source, getImg.alt, getImg.dataset.index);
+
+  window.addEventListener('keyup', closeModalByEsc);
+  window.addEventListener('keyup', pressRightBtn);
+  window.addEventListener('keyup', pressLeftBtn);
+  mainRefs.btnRightArrowRef.addEventListener('click', onRightArrowClick);
+  mainRefs.btnLeftArrowRef.addEventListener('click', onLeftArrowClick);
 }
 
 function closeModal() {
   mainRefs.boxImageRef.setAttribute('src', '');
   mainRefs.boxImageRef.setAttribute('alt', '');
   mainRefs.backDropWrapper.classList.toggle('is-open');
+  window.removeEventListener('keyup', closeModalByEsc);
+  window.removeEventListener('keyup', pressRightBtn);
+  window.removeEventListener('keyup', pressLeftBtn);
+  mainRefs.btnRightArrowRef.removeEventListener('click', onRightArrowClick);
+  mainRefs.btnLeftArrowRef.removeEventListener('click', onLeftArrowClick);
 }
 
 function onBackDropCloseModal(event) {
@@ -110,9 +116,9 @@ function onLeftArrowClick() {
 
 function setImgAttribute(index, count) {
   const newIndex = index + count;
-  if (newIndex === -1) return;
 
-  if (newIndex === gallery.length) return;
+  if (newIndex === gallery.length && newIndex === -1) return;
+
   imgAttribute(
     gallery[newIndex].original,
     gallery[newIndex].description,
